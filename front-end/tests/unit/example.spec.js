703083b1c0f1c6013a4ba561f7e07a320217152d
juguemseok <kalkin2@naver.com>
2020-05-30 17:14:08 +0900

    로그인 페이지 UNIT TEST 추가

--- front-end/tests/unit/example.spec.js
+++ front-end/tests/unit/example.spec.js
@@ -1,5 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import LoginPage from '../../src/views/LoginPage'
+import Vue from 'vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
@@ -8,5 +10,14 @@ describe('HelloWorld.vue', () => {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
+  })
+})
+
+describe('LoginPage.vue',() => {
+  it('로그인 페이지가 정상 렌더링 된다', () => {
+    const Constructor = Vue.extend(LoginPage)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('h1').textContent)
+      .toEqual('TaskAgile')
   })
 })
