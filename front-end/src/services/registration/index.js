551887a5852e0e0266ca71eb4c2c4ee4c72ccf85
MZ01-GSJU <mz01-gsju@MacBook-Pro.local>
2020-06-04 16:30:58 +0900

    space error 수

--- front-end/src/services/registration/index.js
+++ front-end/src/services/registration/index.js
@@ -7,7 +7,7 @@ export default {
    */
   register (detail) {
     return new Promise((resolve, reject) => {
-      axios.post('/registrations', detail).then(({data}) => {
+      axios.post('/registrations', detail).then(({ data }) => {
         resolve(data)
       }).catch((error) => {
         reject(error)
